d13fcdd86da85e0dcdc2d41dcd704f840ff7c5e3
Chaoneng Tan <chaonengtan@gmail.com>
2022-06-20 17:11:48 -0700

    useSelector, useDispatch example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { setValue } from './features/userSlice';
 
 function App() {
+  const dispatch = useDispatch()
+  const data = useSelector(e => e.user.value)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? data.name : ''}
+      <input onChange={e => {
+        dispatch(setValue({ name: e.target.value }))
+      }}></input>
     </div>
   );
 }
